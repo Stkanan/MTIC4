c95ffaea1b1eca53bf93e5c137eb20592a096fbc
Samer Kanan <stkanan@hotmail.com>
2021-11-28 13:39:36 -0500

    test 25 nov

--- backend/src/models/facultad.modelo.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const {Schema}= mongoose
- 
-const FacultadSchema = new Schema({
-IDFacultadnombre:String,
-nombreFac:String,
-})
- 
-module.exports= mongoose.model('facultad',FacultadSchema)
